cfbb0791251a0a6c7e6e4c6c8f4b218d3f2bb63f
patrickroux <patrickroux@172.20.10.2>
2013-11-14 18:49:38 +0100

    First push

---
+++ header.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link href='http://fonts.googleapis.com/css?family=Exo:300,400,600' rel='stylesheet' type='text/css'>
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div class="social-media-top">
+    <ul class="social_media_icons">
+        <li>
+            <a target="_blank" href="http://www.twitter.com/ClubCML" title="Twitter">
+                <img src="http://communitymanagerslyon.com/wp-content/themes/rttheme17/images/assets/social_media/icon-twitter.png" width="24" height="24" alt="" style="opacity: 1;">
+                <div class="social_tip" style="display: none; opacity: 0;">Twitter</div>
+            </a>
+        </li>
+        <li>
+            <a target="_blank" href="https://www.facebook.com/groups/Clubdescommunitymanagersdelyon/" title="Facebook">
+                <img src="http://communitymanagerslyon.com/wp-content/themes/rttheme17/images/assets/social_media/icon-facebook.png" width="24" height="24" alt="" style="opacity: 1;">
+                <div class="social_tip" style="display: none; opacity: 0;">Facebook</div>
+            </a>
+        </li>
+    </ul>
+</div>
+<div id="container">
+    <div class="transparent-line"></div>
+    <div class="content-wrapper">
+        <header id="masthead" class="site-header clearfix" role="banner">
+            <div id="logo" class="clearfix   ">
+                <div class="shadow-left"></div>
+                <div class="logo-holder">
+                    <div class="transparent-line logo"></div>
+                    <div class="logo-background">
+                        <a href="http://communitymanagerslyon.com" title="Club des Community Managers de Lyon"><img src="http://communitymanagerslyon.com/wp-content/uploads/2013/04/logo_CML_final.png" alt="Club des Community Managers de Lyon" class="png"></a>
+                    </div>
+                </div>
+                <div class="shadow-right"></div>
+            </div>
+
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
+                <a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+            </nav><!-- #site-navigation -->
+
+            <?php $header_image = get_header_image();
+            if ( ! empty( $header_image ) ) : ?>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
+            <?php endif; ?>
+        </header>
+    </div>
+</div>
+<div id="page" class="hfeed site clear">
+
---
+++ page-templates/front-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Front Page Template
+ *
+ * Description: A page template that provides a key component of WordPress as a CMS
+ * by meeting the need for a carefully crafted introductory page. The front page template
+ * in Twenty Twelve consists of a page content area for adding text, images, video --
+ * anything you'd like -- followed by front-page-only widgets in one or two columns.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+<div class="slider">
+    <?php if ( function_exists('show_nivo_slider') ) { show_nivo_slider(); } ?>
+</div>
+<div class="banner-holder clearfix">
+    <div class="banner clearfix">
+        <a href="#" class="banner-button alignright">En savoir plus</a>
+        <div class="featured-text withbutton">
+            <p>Une association créée par et pour des community managers Lyonnais. <br> Nos objectifs : l'échange de bonnes pratiques et la promotion du métier.</p>
+        </div>
+    </div>
+</div>
+<div id="main" class="wrapper">
+
+<?php get_sidebar( 'front' ); ?>
+<?php get_footer(); ?>
