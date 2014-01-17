df4860d10456f9f6664b04534a2a592ebf1185fb
Manoz <kevin@manoz.fr>
2014-01-17 16:40:50 +0100

    Better code (remove alot of <?php ?> )

--- single-interview.php
+++ single-interview.php
@@ -10,18 +10,21 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content">
+
 		<div id="content" role="main" class="interview">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+                while ( have_posts() ) : the_post();
 
-				<?php get_template_part( 'content', 'interview' ); ?>
+				    get_template_part( 'content', 'interview' );
 
-				<?php comments_template( '', true ); ?>
+				    comments_template( '', true );
 
-			<?php endwhile; // end of the loop. ?>
+			     endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
